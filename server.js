cfdee9c1a05ebcf351957dd6cb6b9d288821381c
Jakub <staniojakub@gmail.com>
2019-02-02 15:35:06 +0100

    Add cors

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const cors = require('cors');
 const schema = require('./schema');
 
 // initialize express
 const app = express();
+
+// allow cross-origin
+app.use(cors());
 
 app.use('/graphql', graphqlHTTP({
   schema,
