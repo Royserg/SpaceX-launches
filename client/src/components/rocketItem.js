67e817385eaf24c44b401427992d3434ede76ef4
Jakub <staniojakub@gmail.com>
2019-02-04 12:04:03 +0100

    Change col size

--- client/src/components/rocketItem.js
+++ client/src/components/rocketItem.js
@@ -6,7 +6,7 @@ export default function RocketItem({ rocket: { rocket_name, active, rocket_type,
   return (
     <div className='card card-body mb-3'>
       <div className="row">
-        <div className="col-md-9">
+        <div className="col-md-10">
           <h4>
             Rocket:
             <span className={classNames({
@@ -19,7 +19,7 @@ export default function RocketItem({ rocket: { rocket_name, active, rocket_type,
           </h4>
           <p>Rocket Type: {rocket_type}</p>
         </div>
-        <div className="col-md-3">
+        <div className="col-md-2">
           <Link to={`/rockets/${rocket_id}`} className="btn btn-primary">Rocket Details</Link>
         </div>
       </div>
